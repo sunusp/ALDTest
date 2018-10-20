cb15534afc07ff6aa0ed94507e015f880084aebb
Sunu Surendran <sunusp@gmail.com>
2018-10-20 17:06:31 +0100

    files

---
+++ ALDQuoteService/QuoteEngines/HPFinanceCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace ALDQuoteService.QuoteEngines
+{
+    public class HPFinanceCalculator : FinanceCalculatorBase
+    {
+        /// <summary>
+        /// Calculates the APR (interest rate) applicable to specified contract term.
+        /// </summary>
+        /// <param name="termMonths">The contract term in months</param>
+        /// <returns></returns>
+        public override decimal GetAnnualPercentageRate(short termMonths)
+        {
+            // TODO - Implement specific APR calculation for HP contracts
+            return base.GetAnnualPercentageRate(termMonths);
+        }
+    }
+}
